acb4257cbeb865aca0e8a8b94e7937918a72bd3e
Hritik Chauhan <hritikchji@gmail.com>
2025-11-03 18:33:03 +0530

    update live page

--- frontend/app/components/LiveReadings.js
+++ frontend/app/components/LiveReadings.js
@@ -44,7 +44,7 @@ export default function LiveReadings({ data, isConnected }) {
           )}
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
           {data.map((device) => (
             <DeviceCard key={device.device_id} data={device} />
           ))}
